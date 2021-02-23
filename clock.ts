1acffb7c0da2f579bd1e70a84e851a031306ba25
Leon Segal <leonsegal1@gmail.com>
2021-02-23 23:11:12 +0000

    add live clock

--- clock.ts
+++ clock.ts
@@ -1,7 +1,9 @@
 export async function setClock() {
   const clock = document.getElementById("clockHolder");
-  const date = new Date();
-  const dateString = date.toLocaleDateString();
-  const timeString = date.toLocaleTimeString();
-  clock.innerText = `${dateString} ${timeString}`;
+  setInterval(() => {
+    const date = new Date();
+    const dateString = date.toDateString();
+    const timeString = date.toLocaleTimeString();
+    clock.innerText = `${dateString} ${timeString}`;
+  }, 1000);
 }
