259c55f2990220e0ece1cabfb3acc926c65b32ee
Leon Segal <leonsegal1@gmail.com>
2021-02-23 10:10:57 +0000

    add display fn

--- app.js
+++ app.js
@@ -1,18 +1,15 @@
-// initialise
-let current = ["1", "0", "0"];
-const pageHolder = document.getElementById("numberHolder");
-const dummyHolder = document.getElementById("dummyHolder");
-pageHolder.innerText = current.join("");
+let current, target, pageHolder, displayHolder;
 
-let target = [];
+initialiseItems();
+display(current, pageHolder);
 
 document.addEventListener("keypress", (event) => {
   if (event.code.match(/^Digit[0-9]$/)) {
     target.push(event.code.replace("Digit", ""));
-    display(target);
+    display(target, pageHolder);
 
     if (target.length === 3) {
-      display(current);
+      display(current, pageHolder);
 
       const targetNumber = Number(target.join(""));
       let currentNumber = Number(current.join(""));
@@ -21,26 +18,40 @@ document.addEventListener("keypress", (event) => {
         if (currentNumber < targetNumber) {
           currentNumber += 1;
 
-          pageHolder.innerText = currentNumber;
+          display(currentNumber, pageHolder);
         }
 
         if (currentNumber === targetNumber) {
           clearInterval(timer);
           current = target;
           target = [];
-          display(current);
-          dummyHolder.innerText = current.join(""); // delete me
+          display(current, pageHolder);
+          display(current, displayHolder);
         }
       }, 10);
     }
   }
 });
 
-function display(elem) {
-  pageHolder.innerText = elem.join("");
+function initialiseItems() {
+  current = ["1", "0", "0"];
+  target = [];
+  pageHolder = document.getElementById("numberHolder");
+  displayHolder = document.getElementById("displayHolder");
 }
 
+function display(data, elem) {
+  if (Array.isArray(data)) {
+    elem.innerText = data.join("");
+  } else {
+    elem.innerText = data;
+  }
+}
+
+// Tests:
 // wrong num
 // num recirculate
 // num speed variation
 // no matching page
+// too many numbers typed
+// handle trailing zeroes
