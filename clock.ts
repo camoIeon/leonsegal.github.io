459464c0e509575b1bfd6a8bf2d67838e3f38c0b
Leon Segal <leonsegal1@gmail.com>
2021-02-24 09:42:41 +0000

    initialise clock on start

--- clock.ts
+++ clock.ts
@@ -1,5 +1,10 @@
 export async function setClock() {
   const clock = document.getElementById("clockHolder");
+  const date = new Date();
+  const dateString = date.toDateString();
+  const timeString = date.toLocaleTimeString();
+  clock.innerText = `${dateString} ${timeString}`;
+
   setInterval(() => {
     const date = new Date();
     const dateString = date.toDateString();
